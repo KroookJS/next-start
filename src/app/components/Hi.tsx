5e8bef8701d462a87e759bb2a3102f7394ae120d
user <user@MacBook-Pro-user.local>
2023-06-11 22:57:58 +0300

    first commit

---
+++ src/app/components/Hi.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Hi = () => {
+  return (
+    <div>Hi world! I Lov you!</div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Hi } from "./components/Hi";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Hi />
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
@@ -15,7 +17,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
+            By{" "}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
@@ -91,5 +93,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  )
+  );
 }
