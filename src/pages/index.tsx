5748dc9a00b55be8cb73297c5c3344d98f2584b0
user <user@MacBook-Pro-user.local>
2023-06-11 23:24:10 +0300

    edit page

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Hi } from "../components/Hi";
+
+export default function Home() {
+  return (
+    <>
+      <Hi />
+    </>
+  );
+}
